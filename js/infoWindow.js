aaea48b852e3755d19a42b61107f1dc9698e5d5c
Dmytro Shapoval <dsapoval422@gmail.com>
2023-05-18 02:53:18 +0300

    last-1

--- js/infoWindow.js
+++ js/infoWindow.js
@@ -6,18 +6,11 @@ $(document).ready(() => {
     // info-hidden-wrapper
     $('.info-wrapper').click(() => { //it's our black div with important info onclick it show all info
         if ($('.show-button').hasClass('animate-button')) { //check class and make animation, not toggle because height have animation by Jquery
-            $('.info-show-wrapper').animate({
-                height: '72px'
-            }, 1500)
             $('.show-button').removeClass('animate-button')
 
             $('.info-hidden-wrapper').addClass('hidden');
         }
         else {
-           
-            $('.info-show-wrapper').animate({
-                height: 'auto'
-            }, 1000)
             $('.show-button').addClass('animate-button')
             setTimeout(() => {
                 $('.info-hidden-wrapper').removeClass('hidden');
