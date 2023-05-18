f6a5c1434d657f0c0bb8f99f2d8b531525921ab6
Dmytro Shapoval <dsapoval422@gmail.com>
2023-05-18 03:12:36 +0300

    last

--- js/itemHover.js
+++ js/itemHover.js
@@ -1,9 +1,9 @@
 $(document).ready(()=>{
-    $(document).on("mouseover", ".product-image", (event)=>{ //function that show button under product when it was hover           
+    $(document).on("mouseover", ".first-product-item-wrapper", (event)=>{ //function that show button under product when it was hover           
         let index = event.target.dataset.itemNumber
         $('.product-button').eq(index).removeClass('hidden')
     })
-    $(document).on("mouseleave", ".product-image", ()=>{//function that hide button under product
+    $(document).on("mouseleave", ".first-product-item-wrapper", ()=>{//function that hide button under product
         $('.product-button').addClass('hidden')
     })
 })
